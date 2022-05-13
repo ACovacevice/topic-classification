e6c422c8cfdef30183b6695a250b22f83d55a643
Allan Covacevice <16005266+ACovacevice@users.noreply.github.com>
2022-05-13 11:09:16 -0300

    Update __init__.py

--- topic_classification/__init__.py
+++ topic_classification/__init__.py
@@ -1,1 +1,5 @@
-from .lda import LDAClassifier, LDATransformer
+from ._clf import ClassificationModel
+from ._dict import DictTransformer
+from ._lda import LDATransformer
+from ._pipe import LDAClassifier
+from ._prep import DocTransformer
